b140863a8a328b925736520214453125b43bc68a
RaphaelKaira <113975698+Raphael-pix@users.noreply.github.com>
2025-04-07 20:54:17 +0300

    fixing errors

--- app/(main)/channels/page.tsx
+++ app/(main)/channels/page.tsx
@@ -2,7 +2,7 @@ import React from "react";
 import ChannelsTable from "./_components/channelsTable";
 
 export default async function ChannelsPage({ searchParams }:{searchParams : {location?:string}}) {
-  const { location } = await searchParams;
+  const { location } = searchParams;
   return (
     <div className="p-2 space-y-6 lg:p-6">
       <div className="flex items-center justify-between mb-2">
